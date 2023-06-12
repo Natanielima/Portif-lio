ee03dbdb29719678f63b462e0f59a0bf80df63ee
Natanielima <nataniellima27@gmail.com>
2023-06-12 13:48:50 -0300

    ok

--- app/components/Pages/Home/know-techs/index.tsx
+++ app/components/Pages/Home/know-techs/index.tsx
@@ -19,8 +19,9 @@ export default function KnowTechs({ techs }: KnowTechsProps) {
           whileInView={{opacity:1, x:0}}
           exit={{opacity:0, x:-100}}
           transition={{duration:0.5, delay:i*0.2}}
+          key={tech.name}
           >
-             <KnowTech key={tech.name} tech={tech} />
+             <KnowTech  tech={tech} />
           </motion.div>
          
         ))}
