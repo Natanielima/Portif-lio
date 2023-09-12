5de3f5e1aacfc483408fff56d581b5ab238124b4
Natanielima <nataniellima27@gmail.com>
2023-09-12 10:03:53 -0300

    Correção

--- app/components/Pages/Projects/page-intro/index.tsx
+++ app/components/Pages/Projects/page-intro/index.tsx
@@ -20,12 +20,7 @@ export default function PageIntro() {
       transition={{duration:0.6}}
       className="flex flex-col items-center">
         <p className="text-gray-400 text-center max-w-[640px] my-6 text-sm sm:text-base">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad nulla
-          modi quas officia. Praesentium amet fugit quisquam molestias veniam
-          necessitatibus in odit atque provident ea consequuntur aspernatur
-          minima nam autem nobis recusandae neque tenetur eveniet, vel ratione?
-          Earum, accusamus. Ipsam, eaque iusto! Sit sequi et deleniti distinctio
-          dolorem sapiente hic!
+          Aqui vou mostrar a vocês um pouco dos projetos que eu já trabalhei, tanto projetos privados que serão mostrados apenas as telas como também os projetos públicos que terão acesso.
         </p>
         <Link href="/">
           <HiArrowNarrowLeft size={20} />
